f7858cfb6ab48206c259da2daf7a2b28cf92db0c
beresenkow <147422882+beresenkow@users.noreply.github.com>
2025-06-01 17:07:07 +0200

    feat(todo-blog-app): add froms for todo-creation

--- todo-blog-app/src/app/pages/todo.page.ts
+++ todo-blog-app/src/app/pages/todo.page.ts
@@ -2,10 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component, inject } from "@angular/core";
 import { RouterLink, RouterOutlet } from "@angular/router";
 import { TodoService, Todo } from "../todo.services";
+import { FormsModule } from '@angular/forms';
+import { injectContentFiles } from '@analogjs/content';
+import { BlogPost } from '../models/post';
 
 @Component({
     standalone: true,
-    imports: [CommonModule, RouterOutlet, RouterLink],
+    imports: [CommonModule, FormsModule, RouterOutlet, RouterLink],
     template: `
         <h1 class="page-title">Todos</h1>
 
@@ -13,44 +16,97 @@ import { TodoService, Todo } from "../todo.services";
         <a class="menu-panel-active">Todos</a>
         <a routerLink="/about" class="menu-panel">About Me</a>
 
-        <button (click)="addTodo()">Neue Todo</button>
+        <button (click)="openTodoCreation()" [hidden]="creatingNewTodo">Create new Todo</button>
+
+        <div *ngIf="creatingNewTodo">
+          <label for="title">Todo Title</label>
+          <input type="text" id="title" name="title" [(ngModel)]="title" required/>
+
+          <label for="description">Todo Description</label>
+          <input type="text" id="description" name="description" [(ngModel)]="description" required/>
+
+          <label for="linkedBlog">Todo Description</label>
+          <select id="linkedBlog" name="linkedBlog" [(ngModel)]="linkedBlog">
+            <option value="" disabled selected>Select a Blog entry for this Todo</option>
+            <option *ngFor="let option of options" [value]="option.value">{{ option.label }}</option>
+          </select>
+
+          <button class="submit-class" [disabled]="!isFormValid()" (click)="addTodo()">Add new Todo</button>
+          <button class="submit-class" (click)="resetForms()">Cancel</button>
+        </div>
 
         <ul>
           <li *ngFor="let todo of todos">
-            {{ todo.name }} – {{ todo.description }}
+            {{ todo.title }} – {{ todo.description }}
           </li>
         </ul>
 
         <router-outlet />
     `,
 })
-export default class LandingPage {
-    private todoService = inject(TodoService);
-    todos: Todo[] = [];
+export default class TodoPage {
+  private todoService = inject(TodoService);
+  todos: Todo[] = [];
 
-    constructor() {
-      this.loadTodos();
+  creatingNewTodo: boolean = false;
+  title: string = '';
+  description: string = '';
+  linkedBlog: string = '';
+
+  posts = injectContentFiles<BlogPost>();
+  options: { value: string; label: string }[] = [];
+
+  constructor() {
+    this.loadTodos();
+    this.loadBlogPosts();
+  }
+
+  loadBlogPosts() {
+    this.options.push({ value: '', label: 'None' });
+
+    for (let post of this.posts) {
+      this.options.push({ value: post.attributes.title, label: post.attributes.title });
     }
+  }
 
-    loadTodos() {
-      this.todoService.getAll().subscribe({
-        next: (data) => (this.todos = data),
-        error: (err) => console.error('Could not load todo:', err),
-      });
-    }
+  openTodoCreation() {
+    this.creatingNewTodo = true;
+  }
 
-    addTodo() {
-      const newTodo = {
-        name: 'New Todo',
-        description: 'Make sure to create a new todo today',
-        linkedBlog: 'Die Bedeutung von lebenslangem Lernen',
-      }
+  addTodo() {
+    const newTodo = {
+      title: this.title,
+      description: this.description,
+      linkedBlog: this.linkedBlog,
+    };
 
-      this.todoService.create(newTodo).subscribe({
-        next: (createdTodo) => {
-          this.todos.push(createdTodo);
-        },
-        error: (err) => console.error('Could not cretae new todo:', err),
-      });
-    }
+    this.todoService.create(newTodo).subscribe({
+      next: (createdTodo) => {
+        this.todos.push(createdTodo);
+      },
+      error: (err) => console.error('Could not create new todo:', err),
+    });
+
+    this.resetForms();
+  }
+
+  resetForms() {
+    this.creatingNewTodo = false;
+    this.title = '';
+    this.description = '';
+    this.linkedBlog = '';
+  }
+
+  isFormValid() {
+    return !!this.title && !!this.description;
+  }
+
+  loadTodos() {
+    this.todoService.getAll().subscribe({
+      next: (todos) => {
+        this.todos = todos;
+      },
+      error: (err) => console.error('Could not load todos:', err),
+    });
+  }
 }
