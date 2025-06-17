f9dad25858468f3c6d2e06f144a04709b3d618f0
beresenkow <147422882+beresenkow@users.noreply.github.com>
2025-06-17 13:30:17 +0200

    feat(todo-blog-app): remove user handling from project

--- todo-blog-app/src/server/routes/api/users/index.get.ts
+++
@@ -1,9 +0,0 @@
-import { defineEventHandler } from 'h3';
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
-
-export default defineEventHandler(async (event) => {
-  const users = await prisma.user.findMany();
-  return users;
-});
