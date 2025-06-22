4614f499c8453a181999417cf678d60ce69efe31
beresenkow <147422882+beresenkow@users.noreply.github.com>
2025-06-22 13:36:21 +0200

    feat(todo-blog-app): fix catch-all-route `[...page.not-found].page.ts`

--- todo-blog-app/src/app/pages/[...page-not-found].page.ts
+++ todo-blog-app/src/app/pages/[...page-not-found].page.ts
@@ -1,11 +1,29 @@
+import { RouteMeta } from "@analogjs/router";
+import { injectResponse } from "@analogjs/router/tokens";
 import { Component } from "@angular/core";
-import { RouterLink, RouterOutlet } from "@angular/router";
+import { RouterLink } from "@angular/router";
+
+export const routeMeta: RouteMeta = {
+  title: 'Page Not Found',
+  canActivate: [
+    () => {
+      const response = injectResponse();
+      if (import.meta.env.SSR && response) {
+        response.statusCode = 404;
+        //response.end();
+      }
+      return true;
+    }
+  ]
+};
 
 @Component({
     standalone: true,
-    imports: [],
+    imports: [RouterLink],
     template: `
         <h1>Page not found</h1>
+
+        <a routerLink="/">Home</a>
     `,
 })
 export default class PageNotFoundPage {}
